b6701970199d5bbfb94afa3d92262c7f7283e0bf
Fabio <fabiozagaria@proton.me>
2026-09-08 20:48:38 +0200

    Update UserService.java

--- src/main/java/org/esercizi/verificasec/services/UserService.java
+++ src/main/java/org/esercizi/verificasec/services/UserService.java
@@ -2,11 +2,12 @@ package org.esercizi.verificasec.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.esercizi.verificasec.repository;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserService userService;
+    private final UserRepository userRepository;
 
 
 }
